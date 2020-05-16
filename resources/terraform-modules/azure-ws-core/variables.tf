a2a987ac694d938dc6af87cddbcad20268299655
Gianluca Natali <8244206+gianlucanatali@users.noreply.github.com>
2020-05-16 18:00:59 +0200

    adding resources folder

---
+++ resources/terraform-modules/azure-ws-core/variables.tf
@@ -0,0 +1,63 @@
+variable "name" {
+  description = "The Prefix used for all resources in this example"
+}
+
+variable "participant_count" {
+  description = "How number of participants attending the workshop"
+  type        = number
+}
+
+variable "participant_password" {
+  description = "Password for the admin user, to log in from ssh"
+}
+
+
+# VM Variables
+variable "vm_type" {
+  description = "VM Type"
+}
+
+variable "location" {
+  description = "Location"
+}
+
+# CCloud Variables
+variable "ccloud_bootstrap_servers" {
+  description = "Confluent Cloud username"
+}
+
+variable "ccloud_api_key" {
+  description = "Confluent Cloud password"
+}
+
+variable "ccloud_api_secret" {
+  description = "Confluent Cloud Provider"
+}
+
+variable "ccloud_topics" {
+  description = "Confluent Cloud topics to precreate"
+}
+
+variable "ccloud_sr_endpoint" {
+  description = "Confluent Cloud Schema Registry Endpoint"
+}
+
+variable "ccloud_sr_api_key" {
+  description = "Confluent Cloud Schema Registry API key"
+}
+
+variable "ccloud_sr_api_secret" {
+  description = "Confluent Cloud Schema Registry API Secret"
+}
+
+variable "feedback_form_url" {
+  description = "Feedback Form Url"
+}
+
+variable "bootstrap_vm_template_path" { 
+  description = "File path of the bootstrap_vm.tpl"
+}
+
+variable "bootstrap_docker_template_path" { 
+  description = "File path of the bootstrap_docker.tpl"
+}
